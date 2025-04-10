afbe9538288745ca3f73abbc7443c004addca313
alves.dev <henriquefdalves15@gmail.com>
2025-04-09 21:44:00 -0300

    criei os tipo numericos

--- javascript/2-variaveis.js
+++ javascript/2-variaveis.js
@@ -1,9 +1,25 @@
-let name1 = "So pode chamar uma vez essa variavel";
-name1 = "Pode ser alterado o valor";
+//LET
+let name1 = "So pode chamar uma vez essa variavel"
+name1 = "Pode ser alterado o valor"
 
-const name2 = "Não pode alterar o valor, sempre vai ser o mesmo valor";
+//CONST
+const name2 = "Não pode alterar o valor, sempre vai ser o mesmo valor"
+
+//VAR
+var name = "Teste em JavaScript"
+
+//CAMECASEL
+var namePessoal = "Gabriel"
+
+//Imprimi a variavel
+console.log(`Meu nome é ${namePessoal}`) //Formatação para imprimir a variavel
 
 
-var name = "Teste em JavaScript";
 
-console.log(name)
+//TIPOS NUMERICOS
+
+const idade = 40
+const valor1 = 10.5
+const valor2 = 10.5
+console.log(`Minha idade é ${idade}`)
+console.log(`A soma é ${valor1 + valor2}`)
