3cccc24f7018ab9c04669e87b466af7066182d52
alves.dev <henriquefdalves15@gmail.com>
2025-04-09 21:29:14 -0300

    criei a aula de variaveis

---
+++ javascript/2-variaveis.js
@@ -0,0 +1,9 @@
+let name1 = "So pode chamar uma vez essa variavel";
+name1 = "Pode ser alterado o valor";
+
+const name2 = "Não pode alterar o valor, sempre vai ser o mesmo valor";
+
+
+var name = "Teste em JavaScript";
+
+console.log(name)
