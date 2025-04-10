8f7489f15f344f9037a742ae79e76d14b73ed3c7
alves.dev <henriquefdalves15@gmail.com>
2025-04-09 22:00:19 -0300

    att

--- javascript/2-variaveis.js
+++ javascript/2-variaveis.js
@@ -23,3 +23,21 @@ const valor1 = 10.5
 const valor2 = 10.5
 console.log(`Minha idade é ${idade}`)
 console.log(`A soma é ${valor1 + valor2}`)
+
+
+//TIPOS BOLEAN
+const login = true
+
+
+//DADOS ARRAY
+const frutas = ["Banana","Maça","Abacate"] //São vetores
+console.log("Frutas",frutas[1])
+
+
+//DADOS OBJETOS
+const usuario = {
+    name: "Henrique",
+    lastName: "Alves",
+    idade: 20,
+}
+console.log(usuario.name)
