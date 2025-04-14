644f2bdda909ec6e31fed82bdf704ad3b268b916
alves.dev <henriquefdalves15@gmail.com>
2025-04-14 20:49:53 -0300

    trabalhando com tags

--- javascript/DOM-SELECAO/js/script.js
+++ javascript/DOM-SELECAO/js/script.js
@@ -19,3 +19,15 @@ console.log("classText",classText)
 for(text of classText) {
     text.style.color = "blue"
 }
+
+
+
+//Seleção por tags
+
+
+const titles = document.getElementsByTagName("h2")
+console.log("titles",titles)
+
+for(title of titles) {
+    title.style.color = "red"
+}   
