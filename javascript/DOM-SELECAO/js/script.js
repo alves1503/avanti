e044eba692916dc440a053ee834450089b6aa19c
alves.dev <henriquefdalves15@gmail.com>
2025-04-14 20:33:56 -0300

    criei o ex com dom seleção

---
+++ javascript/DOM-SELECAO/js/script.js
@@ -0,0 +1,9 @@
+//Seleção por ID
+
+const content = document.getElementById("content");
+
+console.log("content",content)
+
+content.innerHTML = `<p>Paragrafo de texto</p>`
+content.style.backgroundColor = "red"
+content.style.fontSize = "50px"
